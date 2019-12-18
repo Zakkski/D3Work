da962185fa94730253d2ffe8b16187e82e91f9dc
Zak Kwiecinski <ZKwiecinski@mathematica-mpr.com>
2019-12-18 15:26:40 -0500

    fix apply cahce removal

---
+++ .NET/DatingApp/DatingApp.API/Dtos/UserForDetailsDto.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForDetailsDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Gender { get; set; }
+        public string KnownAs { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int Age { get; set; }
+        public System.DateTime Created { get; set; }
+        public System.DateTime LastActive { get; set; }
+        public string PhotoUrl { get; set; }
+        public ICollection<PhotosForDetailedDto> Photos { get; set; }
+    }
+}

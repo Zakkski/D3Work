2929466e8da3109058da2c252feda0f4a3035d0a
Zak Kwiecinski <ZKwiecinski@mathematica-mpr.com>
2020-01-23 15:53:34 -0500

    add guad and auth service to ionic airbnb

--- Ionic/IonicAirBnB/src/app/places/discover/place-detail/place-detail.module.ts
+++ Ionic/IonicAirBnB/src/app/places/discover/place-detail/place-detail.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { PlaceDetailPageRoutingModule } from './place-detail-routing.module';
 
 import { PlaceDetailPage } from './place-detail.page';
+import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,8 @@ import { PlaceDetailPage } from './place-detail.page';
     IonicModule,
     PlaceDetailPageRoutingModule
   ],
-  declarations: [PlaceDetailPage]
+  declarations: [PlaceDetailPage, CreateBookingComponent],
+  // Needed if component is used not through routing ie as a modal
+  entryComponents: [CreateBookingComponent]
 })
 export class PlaceDetailPageModule {}
--- Ionic/IonicAirBnB/src/app/places/discover/place-detail/place-detail.page.ts
+++ Ionic/IonicAirBnB/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { NavController, ModalController } from '@ionic/angular';
 import { Place } from '../../place.model';
 import { PlacesService } from '../../places.service';
+import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
 
 @Component({
   selector: 'app-place-detail',
@@ -15,7 +16,8 @@ export class PlaceDetailPage implements OnInit {
   constructor(
             private navCtrl: NavController,
             private route: ActivatedRoute,
-            private placesService: PlacesService) { }
+            private placesService: PlacesService,
+            private modalCtrl: ModalController) { }
 
   ngOnInit() {
       this.route.paramMap.subscribe(paramMap => {
@@ -30,10 +32,22 @@ export class PlaceDetailPage implements OnInit {
 
   onBookPlace() {
       // makes ure that animation is correct
-      this.navCtrl.navigateBack('/places/tabs/discover');
+      //   this.navCtrl.navigateBack('/places/tabs/discover');
       // Wouldn't work if stack of pages is empty
-    //   this.navCtrl.pop();
-    // this.router.navigateByUrl('/places/tabs/discover')
+      //   this.navCtrl.pop();
+      // this.router.navigateByUrl('/places/tabs/discover')
+      this.modalCtrl.create({component: CreateBookingComponent, 
+        componentProps: {selectedPlace: this.place }}).then(element => {
+            element.present();
+            return element.onDidDismiss();
+        })
+        .then(resultData => {
+            console.log(resultData.data, resultData.role);
+            if (resultData.role === 'confirm') {
+                console.log('BOOKED');
+            }
+        });
+
   }
 
 }
